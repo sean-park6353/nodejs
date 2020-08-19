1396513099497171ec9b36e28e577c8fb43b9a99
unknown <package9@naver.com>
2020-08-19 19:23:38 +0900

    pug 모델 추가

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser');
+//! app.engine('pug', require('pug').__express) 코드가 없으면 pug 모듈을 못 찾는 문제가 있다.
+app.engine('pug', require('pug').__express)
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 //* app.use()는 미들웨어를 등록하는 함수이다
 //* 등록 후 다음 미들웨어로 넘기기 위해서는 next() 가 있어야한다
@@ -10,12 +14,16 @@ var cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
+app.get('/first_template', (req, res) => {
+    res.render('first_view');
+});
 
-app.post('/', (req, res) => {
-    var name = req.body.name
-    var age = req.body.age
-    console.log(`4. ${Date.now()}`);
-    res.send(`좋아 `)
-})
+// app.post('/', (req, res) => {
+//     var name = req.body.name
+//     var age = req.body.age
+//     console.log(`4. ${Date.now()}`);
+//     res.send(`좋아 `)
+// })
+
 
 app.listen(3000, () => { console.log('포트에연결'); });
